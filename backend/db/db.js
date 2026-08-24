03ad85a02aed0655417fbd04d978cba72455986e
Zaryab ali <zaryabkhan248@gmail.com>
2026-08-24 12:23:16 +0500

    fix: simplify database connection URI in db.js

--- backend/db/db.js
+++ backend/db/db.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 const dbConnection = async () => {
   try {
-    const mongoConnection = await mongoose.connect(
-      `${process.env.MONGO_URI}/${process.env.MONGO_DB_NAME}`
-    );
+    const mongoConnection = await mongoose.connect(process.env.MONGO_URI);
     console.log("DB connected");
   } catch (err) {
     console.log("Error occur while connecting to database", err);
